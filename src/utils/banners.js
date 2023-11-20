820f1fd17325870434371f98e2e5255a21295fd9
Mauro Vera <Maurovera069@gmail.com>
2023-11-20 17:01:30 -0300

    Cambie los nombres como las propiedades de la API

--- src/utils/banners.js
+++ src/utils/banners.js
@@ -2,51 +2,51 @@ export const banners = [
   {
     icon: 'work',
     title: 'Our Work',
-    // type: 'Work samples & Case studies',
-    type: 'services',
-    background: '#1781E2'
+    // category: 'Work samples & Case studies',
+    category: 'services',
+    color: '#1781E2'
   },
   {
     icon: 'chat_dark',
     title: 'Contact',
-    type: 'Get in touch',
-    background: '#E7E7E7',
+    category: 'Get in touch',
+    color: '#E7E7E7',
     dark: true
   },
   {
     icon: 'pc',
     title: 'Careers',
-    type: '3 Positions Available',
-    background: '#1D1D1D'
+    category: '3 Positions Available',
+    color: '#1D1D1D'
   },
   {
     icon: 'info',
     title: 'About',
-    type: 'company profile',
-    background: '#8B12EB'
+    category: 'company profile',
+    color: '#8B12EB'
   },
   {
     icon: 'industries',
     title: 'Industries',
-    type: 'Areas of expertise',
-    background: '#1CB8A5'
+    category: 'Areas of expertise',
+    color: '#1CB8A5'
   },
   {
     icon: 'union',
     title: 'Services',
-    type: 'services',
-    background: '#1781E2'
+    category: 'services',
+    color: '#1781E2'
   },
   {
     icon: 'blog',
     title: 'Blog',
-    type: 'ipsum',
-    background: '#8B12EB'
+    category: 'ipsum',
+    color: '#8B12EB'
   },
   {
-    icon: "social_media",
+    icon: 'social_media',
     title: 'Social media',
-    type: 'Follow us',
-    background: '#D6308F'
+    category: 'Follow us',
+    color: '#D6308F'
   }
 ]
